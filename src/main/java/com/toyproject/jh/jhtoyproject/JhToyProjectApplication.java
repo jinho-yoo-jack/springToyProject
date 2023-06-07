e417c673dea572a27aa904006719e14c5690f3e9
유진호 <black@yujinhoui-MacBookPro.local>
2023-06-07 17:58:48 +0900

    init

---
+++ src/main/java/com/toyproject/jh/jhtoyproject/JhToyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.jh.jhtoyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhToyProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JhToyProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/toyproject/jh/jhtoyproject/JhToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.jh.jhtoyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhToyProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
